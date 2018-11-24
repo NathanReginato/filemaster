2c138a4bd20acc5aff92360a69e0ff361ff49118
Nathan Reginato <Nathan.reginato@gmail.com>
2018-11-23 19:17:42 -0700

    Created basic file reader code

--- cmd/main.go
+++ cmd/main.go
@@ -2,25 +2,92 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
+
+	"github.com/0xAX/notificator"
+	"github.com/gen2brain/dlgs"
+	"github.com/xor-gate/goexif2/exif"
+	"github.com/xor-gate/goexif2/mknote"
+	yaml "gopkg.in/yaml.v2"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
+var notify *notificator.Notificator
+
+type config struct {
+	Root      string   `yaml:"root-directory"`
+	Structure []string `yaml:"file-structure"`
+	Process   []string `yaml:"process"`
 }
 
 func main() {
 
-	cmd := os.Args[1]
+	t := config{}
 
-	file, err := os.Create("/Users/nathanreginato/Desktop/file.txt")
+	absPath, _ := filepath.Abs("./config.yaml")
+
+	fmt.Println(absPath)
+
+	dat, err := ioutil.ReadFile(absPath)
 	if err != nil {
-		log.Fatal("Cannot create file", err)
+		panic(err)
 	}
-	defer file.Close()
 
-	fmt.Fprintf(file, cmd)
+	yamlerr := yaml.Unmarshal([]byte(dat), &t)
+	if yamlerr != nil {
+		panic(yamlerr)
+	}
+
+	notify = notificator.New(notificator.Options{
+		DefaultIcon: "icon/default.png",
+		AppName:     "My test App",
+	})
+
+	notify.Push("title", "text", "/home/user/icon.png", notificator.UR_CRITICAL)
+
+	files, _, err := dlgs.FileMulti("Select files", "")
+	if err != nil {
+		panic(err)
+	}
+
+	for k, v := range files {
+		fmt.Println(k, v)
+		// dat, err := ioutil.ReadFile(v)
+		// if err != nil {
+		// 	panic(err)
+		// }
+		//fmt.Println(dat)
+		f, err := os.Open(v)
+		if err != nil {
+			log.Fatal(err)
+		}
+		x, err := exif.Decode(f)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		exif.RegisterParsers(mknote.All...)
+		camModel, err := x.Get(exif.Model)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		camMake, err := x.Get(exif.Make)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		tm, _ := x.DateTime()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(camModel)
+		fmt.Println(camMake)
+		fmt.Println(tm)
+	}
+
+	fmt.Println(t)
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,8 @@
 module github.com/NathanReginato/filemaster
+
+require (
+	github.com/0xAX/notificator v0.0.0-20181105090803-d81462e38c21
+	github.com/gen2brain/dlgs v0.0.0-20180629122906-342edb4c68c1
+	github.com/xor-gate/goexif2 v1.1.0
+	gopkg.in/yaml.v2 v2.2.1
+)
